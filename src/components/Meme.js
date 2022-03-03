61899c78594b6ae9e5f8b238f58d8a5c48a549b7
Victoria Kee <victoriakee95@gmail.com>
2022-03-03 13:52:56 -0600

    build out elements for meme comp

---
+++ src/components/Meme.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+export default function Meme() {
+    
+    
+    return (
+        <main>
+            <div className="form">
+                <input 
+                    type="text"
+                    placeholder="Top text"
+                    className="form-input"
+                    name="topText"
+                    value={meme.topText}
+                    onChange={handleChange}
+                />
+                <input 
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form-input"
+                    name="bottomText"
+                    value={meme.bottomText}
+                    onChange={handleChange}
+                />
+                <button 
+                    className="form-button"
+                    onClick={getMemeImage}
+                >
+                    New meme image
+                </button>
+            </div>
+            <div className="meme">
+                <img src={meme.randomImage} className="meme-img" alt='meme img'/>
+                <h2 className="meme-text top">{meme.topText}</h2>
+                <h2 className="meme-text bottom">{meme.bottomText}</h2>
+            </div>
+        </main>
+    )
+}
